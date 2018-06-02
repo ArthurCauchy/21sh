aca86f96a022b0fac4941756ab18109b42607ed6
ArthurCauchy <arthur.cauchy@free.fr>
2018-06-02 16:52:57 +0200

    removed old code in builtin cd

--- srcs/builtin_cd.c
+++ srcs/builtin_cd.c
@@ -6,39 +6,11 @@
 /*   By: acauchy <acauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 10:06:00 by acauchy           #+#    #+#             */
-/*   Updated: 2018/05/02 17:31:46 by acauchy          ###   ########.fr       */
+/*   Updated: 2018/06/02 16:52:09 by arthur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "twenty_one_sh.h"
-
-/*static int	try_cd(char *options, t_env **env, char *path)
-{
-	char	curr_pwd[MAX_PATH_SIZE];
-	char	*curr_pwd_env;
-	int		nocurr;
-
-	curr_pwd_env = NULL;
-	nocurr = 0;
-	if (!getcwd(curr_pwd, MAX_PATH_SIZE)
-			&& !(curr_pwd_env = read_from_env(env, "PWD")))
-		nocurr = 1;
-	if (chdir(path) == -1)
-	{
-		print_chdir_error(path);
-		return (1);
-	}
-	if (nocurr)
-		set_env(env, ft_strdup("OLDPWD"), ft_strdup("/"));
-	else if (curr_pwd_env)
-		set_env(env, ft_strdup("OLDPWD"), curr_pwd_env);
-	else
-		set_env(env, ft_strdup("OLDPWD"), ft_strdup(curr_pwd));
-	if (!getcwd(curr_pwd, MAX_PATH_SIZE))
-		exit_error("getcwd() error");
-	set_env(env, ft_strdup("PWD"), ft_strdup(curr_pwd));
-	return (0);
-}*/
 
 static int	try_cd(char *options, t_env **env, char *path)
 {
